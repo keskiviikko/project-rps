7614509ba2462bbbae2ceb18ef5e688e73c7717a
Rasmus Rajasalo <rarasmus@outlook.com>
2018-11-15 11:44:10 +0900

    Finish project, improve if necessary

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,21 @@
 let footButton = document.querySelector('#foot');
 let cockroachButton = document.querySelector('#cockroach');
-let nuclearButton = document.querySelector('#nuclear bomb');
+let nuclearButton = document.querySelector('#nuclear');
 let para = document.querySelector('#result');
+let score = document.querySelector('#scores');
+let roundresult = document.querySelector('#roundend');
 
 function computerPlay() {
     let options = ['Foot', 'Cockroach', 'Nuclear bomb'];
     let randomIndex = [Math.floor(Math.random() * options.length)];
     return (options[randomIndex]);
 }
+
 let computerSelection = computerPlay();
 let playerSelection;
+
+let computerScore = 0;
+let playerScore = 0;
 
 function playerSelectionFoot() {
     let playerSelection = 'Foot';
@@ -17,12 +23,15 @@ function playerSelectionFoot() {
     if (computerSelection == 'Cockroach') {
         let result = `You chose ${playerSelection}. Computer chose ${computerSelection}. You win!`;
         para.textContent = (result);
+        score.textContent = `Player score: ${++playerScore} | Computer score: ${computerScore}`;
     } else if (computerSelection == 'Nuclear bomb') {
         let result = `You chose ${playerSelection}. Computer chose ${computerSelection}. You lose...`;
         para.textContent = (result);
+        score.textContent = `Player score: ${playerScore} | Computer score: ${++computerScore}`;
     } else if (playerSelection == computerSelection) {
         let result = 'It\'s a tie. Try again.';
         para.textContent = (result);
+        score.textContent = `Player score: ${playerScore} | Computer score: ${computerScore}`;
     } else {
         let result = 'There was a problem. Try again.';
         para.textContent = (result);
@@ -35,12 +44,15 @@ function playerSelectionCockroach() {
     if (computerSelection == 'Nuclear bomb') {
         let result = `You chose ${playerSelection}. Computer chose ${computerSelection}. You win!`;
         para.textContent = (result);
+        score.textContent = `Player score: ${++playerScore} | Computer score: ${computerScore}`;
     } else if (computerSelection == 'Foot') {
         let result = `You chose ${playerSelection}. Computer chose ${computerSelection}. You lose...`;
         para.textContent = (result);
+        score.textContent = `Player score: ${playerScore} | Computer score: ${++computerScore}`;
     } else if (playerSelection == computerSelection) {
         let result = 'It\'s a tie. Try again.';
         para.textContent = (result);
+        score.textContent = `Player score: ${playerScore} | Computer score: ${computerScore}`;
     } else {
         let result = 'There was a problem. Try again.';
         para.textContent = (result);
@@ -53,26 +65,47 @@ function playerSelectionNuclear() {
     if (computerSelection == 'Foot') {
         let result = `You chose ${playerSelection}. Computer chose ${computerSelection}. You win!`;
         para.textContent = (result);
+        score.textContent = `Player score: ${++playerScore} | Computer score: ${computerScore}`;
     } else if (computerSelection == 'Cockroach') {
         let result = `You chose ${playerSelection}. Computer chose ${computerSelection}. You lose...`;
         para.textContent = (result);
+        score.textContent = `Player score: ${playerScore} | Computer score: ${++computerScore}`;
     } else if (playerSelection == computerSelection) {
         let result = 'It\'s a tie. Try again.';
         para.textContent = (result);
+        score.textContent = `Player score: ${playerScore} | Computer score: ${computerScore}`;
     } else {
         let result = 'There was a problem. Try again.';
         para.textContent = (result);
     }
 }
 
+function roundEnd() {
+    console.log({ playerScore, computerScore })
+    if (playerScore == 5) {
+        roundresult.textContent = 'You won this round!';
+        playerScore = 0;
+        computerScore = 0;
+    } else if (computerScore == 5) {
+        roundresult.textContent = 'You lost this round...';
+        playerScore = 0;
+        computerScore = 0;
+    } else {
+        roundresult.textContent = '';
+    }
+}
+
 footButton.onclick = function () {
     playerSelectionFoot();
+    roundEnd();
 }
 
 cockroachButton.onclick = function () {
     playerSelectionCockroach();
+    roundEnd();
 }
 
 nuclearButton.onclick = function () {
     playerSelectionNuclear();
+    roundEnd();
 }
